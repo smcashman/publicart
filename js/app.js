a65d06b5c2f70f317abdfa5de3fa06480f655b61
Sara Cashman <saramcashman@gmail.com>
2016-08-29 22:31:39 -0400

    more categories

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 
 
-
 $(document).ready(function() {
     $("#citysearch").click(function(){
         var userSearch = $("#placesearch").val();
@@ -8,24 +7,32 @@ $(document).ready(function() {
     
         $.getJSON("https://api.foursquare.com/v2/venues/search?v=20131016&near="+userSearch+"&intent=browse&radius=800&categoryId=507c8c4091d498d9fc8c67a9&client_id=C2IEUINHBL2HEVOHIWNO0GGN5EUHK3PGYH03HCZRP2ETA4CF&client_secret=DOLF3UBQZOY5GX2DP3EXBQ5CW4AHEWMNDGRMH0IHJWZBDSIO", function(data){
         var myResponse = (data.response);
-        console.log(myResponse);
+        //console.log(myResponse);
         
-        $.getJSON("https://api.foursquare.com/v2/venues/search?v=20131016&near="+userSearch+"&intent=browse&radius=800&categoryId=4bf58dd8d48988d1e2931735&client_id=C2IEUINHBL2HEVOHIWNO0GGN5EUHK3PGYH03HCZRP2ETA4CF&client_secret=DOLF3UBQZOY5GX2DP3EXBQ5CW4AHEWMNDGRMH0IHJWZBDSIO", function(datta){
-        var Response = (datta.response);
-        console.log(Response);
+        $.getJSON("https://api.foursquare.com/v2/venues/search?v=20131016&near="+userSearch+"&intent=browse&radius=800&categoryId=4bf58dd8d48988d18f941735&client_id=C2IEUINHBL2HEVOHIWNO0GGN5EUHK3PGYH03HCZRP2ETA4CF&client_secret=DOLF3UBQZOY5GX2DP3EXBQ5CW4AHEWMNDGRMH0IHJWZBDSIO", function(artmuseum){
+        var Response = (artmuseum.response);
+        //console.log(Response);
+
+        $.getJSON("https://api.foursquare.com/v2/venues/search?v=20131016&near="+userSearch+"&intent=browse&radius=800&categoryId=4bf58dd8d48988d1e2931735&client_id=C2IEUINHBL2HEVOHIWNO0GGN5EUHK3PGYH03HCZRP2ETA4CF&client_secret=DOLF3UBQZOY5GX2DP3EXBQ5CW4AHEWMNDGRMH0IHJWZBDSIO", function(artgallery){
+        var ArtResponse = (artgallery.response);
+        console.log(ArtResponse);
 
       var locations = []
       var centerpoint = myResponse.venues[0].location.lat
       var centerlat = myResponse.venues[0].location.lng
       console.log(centerpoint);
       console.log(centerlat);
+
+      $.each(Response.venues,function(index,value){
+        locations.push([this.name,this.location.lat,this.location.lng]);
+      })
+
        $.each(myResponse.venues,function(index,value){
-            $('.foursquarebox').append("<p class=placeName>"+this.name+"</p>");
-            $('.foursquarebox').append("<p class='latlong'>"+this.location.address+", "+this.location.city+"</p>");
-           
-       
+            locations.push([this.name,this.location.lat,this.location.lng])
+        });
+       $.each(ArtResponse.venues,function(index,value){
         locations.push([this.name,this.location.lat,this.location.lng])
-    });
+       })
         console.log(locations);
 
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -53,48 +60,10 @@ $(document).ready(function() {
     }
     });
 });
-    /*var locations = [
-      ['Bondi Beach', -33.890542, 151.274856, 4],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
-      ['Cronulla Beach', -34.028249, 151.157507, 3],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];*/
+    });
     });
 
-   /* var userSearch=("");
-$("#citysearch").click(function(){
-        userSearch = $("#placesearch").val();
-        console.log(userSearch);
-    });
-//4d4b7105d754a06374d81259
-    $.getJSON('https://api.foursquare.com/v2/venues/search?v=20131016&near=boston&intent=browse&radius=800&categoryId=507c8c4091d498d9fc8c67a9&client_id=C2IEUINHBL2HEVOHIWNO0GGN5EUHK3PGYH03HCZRP2ETA4CF&client_secret=DOLF3UBQZOY5GX2DP3EXBQ5CW4AHEWMNDGRMH0IHJWZBDSIO', function(data){
-        var myResponse = (data.response);
-        console.log(myResponse);
-      
-      
-       $.each(myResponse.venues,function(index,value){
-            $('.foursquarebox').append("<p class=placeName>"+this.name+"</p>");
-            $('.foursquarebox').append("<p class='latlong'>"+this.location.address+", "+this.location.city+"</p>");
-            var artSpot = (this.location.address);
-            console.log(artSpot);
-            //var placeLat = $(this.location).val();
-            //console.log(placeLat);
-            });
-      
-    
-           
-    
-           
-    
-    });
-
-
-//$.getJSON("https://maps.googleapis.com/maps/api/js?key=AIzaSyBzVrwxmmY47YJMMxtBnR57Af751GqwOG0&callback=initMap", function(street){
-    //$('.googlebox').append("<img src=" +street+">");
-    
-  //});
-*/
+  
 });
 
 
